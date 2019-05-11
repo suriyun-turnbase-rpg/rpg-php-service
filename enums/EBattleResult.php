e96d8c65e3718028839e7d86271b1ba13d498aad
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2019-05-12 05:20:22 +0700

    Add an enums

---
+++ enums/EBattleResult.php
@@ -0,0 +1,8 @@
+<?php
+final class EBattleResult extends BasicEnum
+{
+	const None = 0;
+	const Lose = 1;
+	const Win = 2;
+}
+?>
---
+++ enums/EFormationType.php
@@ -0,0 +1,7 @@
+<?php
+final class EFormationType extends BasicEnum
+{
+	const Stage = 0;
+	const Arena = 1;
+}
+?>
