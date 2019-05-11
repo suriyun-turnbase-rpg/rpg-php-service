f484f69c588af8edde5070184bd87f9ca217ed99
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2019-05-12 05:11:37 +0700

    Remove config.php

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Database connection
-// TODO: May use environment configs
-$mysql_host = 'localhost';
-$mysql_port = 3306;
-$mysql_dbname = 'tbrpg';
-$mysql_username = 'root';
-$mysql_password = NULL;
-?>
