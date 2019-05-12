da575ee9c640c258766e04ed47140afbdb2a107c
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2019-05-12 08:57:33 +0700

    Add stamina unit / lootbox requirement type enums

---
+++ enums/ELootboxRequirementType.php
@@ -0,0 +1,7 @@
+<?php
+final class ELootboxRequirementType extends BasicEnum
+{
+	const SoftCurrency = 0;
+	const HardCurrency = 1;
+}
+?>
---
+++ enums/EStaminaUnit.php
@@ -0,0 +1,9 @@
+<?php
+final class EStaminaUnit extends BasicEnum
+{
+	const Seconds = 0;
+	const Minutes = 1;
+	const Hours = 1;
+	const Days = 1;
+}
+?>
