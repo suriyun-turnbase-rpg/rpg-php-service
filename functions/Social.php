3be2dc04c578084a22cb5920fd15615332147003
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2019-05-12 10:59:18 +0700

    WIP social functions

---
+++ functions/Social.php
@@ -0,0 +1,105 @@
+<?php
+function FriendRequest($targetPlayerId)
+{
+    $player = GetPlayer();
+    $playerId = $player->id;
+    $playerFriendDb = new PlayerFriend();
+    $playerFriend = $playerFriendDb->load(array(
+        'playerId = ? AND targetPlayerId = ?',
+        $playerId,
+        $targetPlayerId
+    ));
+
+    $playerFriendRequestDb = new PlayerFriendRequest();
+    $playerFriendRequest = $playerFriendRequestDb->load(array(
+        'playerId = ? AND targetPlayerId = ?',
+        $playerId,
+        $targetPlayerId
+    ));
+    
+    if (!$playerFriend && !$playerFriendRequest)
+    {
+        $newRequest = new PlayerFriendRequest();
+        $newRequest->playerId = $playerId;
+        $newRequest->targetPlayerId = $targetPlayerId;
+        $newRequest->save();
+    }
+}
+
+function FriendAccept($targetPlayerId)
+{
+    $player = GetPlayer();
+    $playerId = $player->id;
+    // Validate request
+    $playerFriendRequestDb = new PlayerFriendRequest();
+    if ($playerFriendRequestDb->load(array(
+        'playerId = ? AND targetPlayerId = ?',
+        $targetPlayerId,
+        $playerId
+    )))
+    {
+        // Remove requests
+        $playerFriendRequestDb = new PlayerFriendRequest();
+        $playerFriendRequestDb->erase(array(
+            '(playerId = ? AND targetPlayerId = ?) OR (playerId = ? AND targetPlayerId = ?)',
+            $playerId,
+            $targetPlayerId,
+            $targetPlayerId,
+            $playerId
+        ));
+        // Add friend
+        $playerFriendA = new PlayerFriend();
+        $playerFriendA->playerId = $playerId;
+        $playerFriendA->targetPlayerId = $targetPlayerId;
+        $playerFriendA->save();
+        // B
+        $playerFriendB = new PlayerFriend();
+        $playerFriendB->playerId = $targetPlayerId;
+        $playerFriendB->targetPlayerId = $playerId;
+        $playerFriendB->save();
+    }
+}
+
+function FriendDecline($targetPlayerId)
+{
+    $player = GetPlayer();
+    $playerId = $player->id;
+    // Validate request
+    $playerFriendRequestDb = new PlayerFriendRequest();
+    if ($playerFriendRequestDb->load(array(
+        'playerId = ? AND targetPlayerId = ?',
+        $targetPlayerId,
+        $playerId
+    )))
+    {
+        // Remove requests
+        $playerFriendRequestDb = new PlayerFriendRequest();
+        $playerFriendRequestDb->erase(array(
+            '(playerId = ? AND targetPlayerId = ?) OR (playerId = ? AND targetPlayerId = ?)',
+            $playerId,
+            $targetPlayerId,
+            $targetPlayerId,
+            $playerId
+        ));
+    }
+}
+
+function FriendDelete($targetPlayerId)
+{
+    $player = Spark.getPlayer();
+    $playerId = player.getPlayerId();
+    $playerFriendDb = new PlayerFriend();
+    $playerFriendDb->erase(array(
+        '(playerId = ? AND targetPlayerId = ?) OR (playerId = ? AND targetPlayerId = ?)',
+        $playerId,
+        $targetPlayerId,
+        $targetPlayerId,
+        $playerId
+    ));
+}
+
+function FindUser(displayName)
+{
+    
+}
+?>
