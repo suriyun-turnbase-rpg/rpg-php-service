d4d0752b18eb42db9ab3e17e220237052a801c84
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2019-05-16 12:10:45 +0700

    Add sql-importer

---
+++ sql-importer.php
@@ -0,0 +1,89 @@
+<?php
+$f3 = require_once('fatfree/lib/base.php');
+
+if ((float)PCRE_VERSION < 7.9) {
+    trigger_error('PCRE version is out of date');
+}
+
+// Read configs
+$f3->config('configs/config.ini');
+
+function importSqlFile($pdo, $sqlFile, $tablePrefix = null)
+{
+	try {
+		
+		// Enable LOAD LOCAL INFILE
+		$pdo->setAttribute(\PDO::MYSQL_ATTR_LOCAL_INFILE, true);
+		
+		$errorDetect = false;
+		
+		// Temporary variable, used to store current query
+		$tmpLine = '';
+		
+		// Read in entire file
+		$lines = file($sqlFile);
+		
+		// Loop through each line
+		foreach ($lines as $line) {
+			// Skip it if it's a comment
+			if (substr($line, 0, 2) == '--' || trim($line) == '') {
+				continue;
+			}
+			
+			// Read & replace prefix
+			$line = str_replace(['<<__prefix__>>'], [$tablePrefix], $line);
+			
+			// Add this line to the current segment
+			$tmpLine .= $line;
+			
+			// If it has a semicolon at the end, it's the end of the query
+			if (substr(trim($line), -1, 1) == ';') {
+				try {
+					// Perform the Query
+					$pdo->exec($tmpLine);
+				} catch (\PDOException $e) {
+					echo "<br><pre>Error performing Query: '<strong>" . $tmpLine . "</strong>': " . $e->getMessage() . "</pre>\n";
+					$errorDetect = true;
+				}
+				
+				// Reset temp variable to empty
+				$tmpLine = '';
+			}
+		}
+		
+		// Check if error is detected
+		if ($errorDetect) {
+			return false;
+		}
+		
+	} catch (\Exception $e) {
+		echo "<br><pre>Exception => " . $e->getMessage() . "</pre>\n";
+		return false;
+	}
+	
+	return true;
+}
+
+// Prepare to import
+// PDO DSN
+$dsn = 'mysql:host='.$f3->db_host.
+    ';port='.$f3->db_port.
+    ';dbname='.$f3->db_name.
+    ';charset=utf8';
+// PDO options
+$options = [
+    PDO::ATTR_EMULATE_PREPARES   => false, // turn off emulation mode for "real" prepared statements
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION, //turn on errors in the form of exceptions
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, //make the default fetch be an associative array
+];
+
+// Start import
+try {
+    $pdo = new PDO($dsn, $f3->db_user, $f3->db_pass, $options);
+    importSqlFile($pdo, './sqls/mysql_main.sql', $f3->db_prefix);
+    echo 'Done ;)';
+} catch (Exception $e) {
+    error_log($e->getMessage());
+    exit('Cannot connect to database server, check your config in configs/config.ini');
+}
+?>
--- sqls/mysql_main.sql
+++ sqls/mysql_main.sql
@@ -28,7 +28,7 @@ SET time_zone = "+00:00";
 -- Table structure for table `player`
 --
 
-CREATE TABLE `player` (
+CREATE TABLE `<<__prefix__>>player` (
   `id` bigint(20) NOT NULL,
   `profileName` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `loginToken` varchar(512) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
@@ -50,7 +50,7 @@ CREATE TABLE `player` (
 -- Table structure for table `player_auth`
 --
 
-CREATE TABLE `player_auth` (
+CREATE TABLE `<<__prefix__>>player_auth` (
   `id` bigint(20) NOT NULL,
   `playerId` bigint(20) NOT NULL,
   `type` tinyint(4) NOT NULL,
@@ -66,7 +66,7 @@ CREATE TABLE `player_auth` (
 -- Table structure for table `player_battle`
 --
 
-CREATE TABLE `player_battle` (
+CREATE TABLE `<<__prefix__>>player_battle` (
   `id` bigint(20) NOT NULL,
   `playerId` bigint(20) NOT NULL,
   `dataId` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
@@ -84,7 +84,7 @@ CREATE TABLE `player_battle` (
 -- Table structure for table `player_clear_stage`
 --
 
-CREATE TABLE `player_clear_stage` (
+CREATE TABLE `<<__prefix__>>player_clear_stage` (
   `id` bigint(20) NOT NULL,
   `playerId` bigint(20) NOT NULL,
   `dataId` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
@@ -99,7 +99,7 @@ CREATE TABLE `player_clear_stage` (
 -- Table structure for table `player_currency`
 --
 
-CREATE TABLE `player_currency` (
+CREATE TABLE `<<__prefix__>>player_currency` (
   `id` bigint(20) NOT NULL,
   `playerId` bigint(20) NOT NULL,
   `dataId` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
@@ -115,7 +115,7 @@ CREATE TABLE `player_currency` (
 -- Table structure for table `player_formation`
 --
 
-CREATE TABLE `player_formation` (
+CREATE TABLE `<<__prefix__>>player_formation` (
   `id` bigint(20) NOT NULL,
   `playerId` bigint(20) NOT NULL,
   `dataId` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
@@ -131,7 +131,7 @@ CREATE TABLE `player_formation` (
 -- Table structure for table `player_friend`
 --
 
-CREATE TABLE `player_friend` (
+CREATE TABLE `<<__prefix__>>player_friend` (
   `id` bigint(20) NOT NULL,
   `playerId` bigint(20) NOT NULL,
   `targetPlayerId` bigint(20) NOT NULL,
@@ -145,7 +145,7 @@ CREATE TABLE `player_friend` (
 -- Table structure for table `player_friend_request`
 --
 
-CREATE TABLE `player_friend_request` (
+CREATE TABLE `<<__prefix__>>player_friend_request` (
   `id` bigint(20) NOT NULL,
   `playerId` bigint(20) NOT NULL,
   `targetPlayerId` bigint(20) NOT NULL,
@@ -159,7 +159,7 @@ CREATE TABLE `player_friend_request` (
 -- Table structure for table `player_item`
 --
 
-CREATE TABLE `player_item` (
+CREATE TABLE `<<__prefix__>>player_item` (
   `id` bigint(20) NOT NULL,
   `playerId` bigint(20) NOT NULL,
   `dataId` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
@@ -177,7 +177,7 @@ CREATE TABLE `player_item` (
 -- Table structure for table `player_stamina`
 --
 
-CREATE TABLE `player_stamina` (
+CREATE TABLE `<<__prefix__>>player_stamina` (
   `id` bigint(20) NOT NULL,
   `playerId` bigint(20) NOT NULL,
   `dataId` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
@@ -193,7 +193,7 @@ CREATE TABLE `player_stamina` (
 -- Table structure for table `player_unlock_item`
 --
 
-CREATE TABLE `player_unlock_item` (
+CREATE TABLE `<<__prefix__>>player_unlock_item` (
   `id` bigint(20) NOT NULL,
   `playerId` bigint(20) NOT NULL,
   `dataId` varchar(50) NOT NULL,
@@ -209,67 +209,67 @@ CREATE TABLE `player_unlock_item` (
 --
 -- Indexes for table `player`
 --
-ALTER TABLE `player`
+ALTER TABLE `<<__prefix__>>player`
   ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `player_auth`
 --
-ALTER TABLE `player_auth`
+ALTER TABLE `<<__prefix__>>player_auth`
   ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `player_battle`
 --
-ALTER TABLE `player_battle`
+ALTER TABLE `<<__prefix__>>player_battle`
   ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `player_clear_stage`
 --
-ALTER TABLE `player_clear_stage`
+ALTER TABLE `<<__prefix__>>player_clear_stage`
   ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `player_currency`
 --
-ALTER TABLE `player_currency`
+ALTER TABLE `<<__prefix__>>player_currency`
   ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `player_formation`
 --
-ALTER TABLE `player_formation`
+ALTER TABLE `<<__prefix__>>player_formation`
   ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `player_friend`
 --
-ALTER TABLE `player_friend`
+ALTER TABLE `<<__prefix__>>player_friend`
   ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `player_friend_request`
 --
-ALTER TABLE `player_friend_request`
+ALTER TABLE `<<__prefix__>>player_friend_request`
   ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `player_item`
 --
-ALTER TABLE `player_item`
+ALTER TABLE `<<__prefix__>>player_item`
   ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `player_stamina`
 --
-ALTER TABLE `player_stamina`
+ALTER TABLE `<<__prefix__>>player_stamina`
   ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `player_unlock_item`
 --
-ALTER TABLE `player_unlock_item`
+ALTER TABLE `<<__prefix__>>player_unlock_item`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -279,67 +279,67 @@ ALTER TABLE `player_unlock_item`
 --
 -- AUTO_INCREMENT for table `player`
 --
-ALTER TABLE `player`
+ALTER TABLE `<<__prefix__>>player`
   MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `player_auth`
 --
-ALTER TABLE `player_auth`
+ALTER TABLE `<<__prefix__>>player_auth`
   MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `player_battle`
 --
-ALTER TABLE `player_battle`
+ALTER TABLE `<<__prefix__>>player_battle`
   MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `player_clear_stage`
 --
-ALTER TABLE `player_clear_stage`
+ALTER TABLE `<<__prefix__>>player_clear_stage`
   MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `player_currency`
 --
-ALTER TABLE `player_currency`
+ALTER TABLE `<<__prefix__>>player_currency`
   MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `player_formation`
 --
-ALTER TABLE `player_formation`
+ALTER TABLE `<<__prefix__>>player_formation`
   MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `player_friend`
 --
-ALTER TABLE `player_friend`
+ALTER TABLE `<<__prefix__>>player_friend`
   MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `player_friend_request`
 --
-ALTER TABLE `player_friend_request`
+ALTER TABLE `<<__prefix__>>player_friend_request`
   MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `player_item`
 --
-ALTER TABLE `player_item`
+ALTER TABLE `<<__prefix__>>player_item`
   MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `player_stamina`
 --
-ALTER TABLE `player_stamina`
+ALTER TABLE `<<__prefix__>>player_stamina`
   MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `player_unlock_item`
 --
-ALTER TABLE `player_unlock_item`
+ALTER TABLE `<<__prefix__>>player_unlock_item`
   MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
