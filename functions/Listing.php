a12395ee91db19e5305f203bf761898751ae86cf
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2019-05-16 13:45:54 +0700

    Implement social functions (untested)

--- functions/Listing.php
+++ functions/Listing.php
@@ -77,8 +77,35 @@ function GetHelperList()
     $player = GetPlayer();
     $playerId = $player->id;
     $list = array();
-    // TODO: Implement this
-    echo json_encode(array('list' => CursorsToArray($list)));
+    // TODO: Improve this
+    $db = \Base::instance()->get('DB');
+    $prefix = \Base::instance()->get('db_prefix');
+    $rows = $db->exec('SELECT ' . $prefix . 'player.id AS id, ' .
+        $prefix . 'player_friend.targetPlayerId AS targetPlayerId FROM ' .
+        $prefix . 'player RIGHT JOIN ' . $prefix . 'player_friend ON ' .
+        $prefix . 'player.id = ' . $prefix . 'player_friend.playerId WHERE '.
+        $prefix . 'player.id = :playerId',
+        array(':playerId' => $playerId));
+    // Add list
+    foreach ($rows as $row) {
+        $socialPlayer = GetSocialPlayer($playerId, $row['targetPlayerId']);
+        if ($socialPlayer) {
+            $list[] = $socialPlayer;
+        }
+    }
+    // If helpers not enough, fill more
+    $countRows = count($rows);
+    $limit = 25 - $countRows;
+    if ($limit > 0) {
+        $rows = $db->exec('SELECT id FROM ' . $prefix . 'player WHERE profileName != "" ORDER BY rand() LIMIT ' . $limit);
+        foreach ($rows as $row) {
+            $socialPlayer = GetSocialPlayer($playerId, $row['id']);
+            if ($socialPlayer) {
+                $list[] = $socialPlayer;
+            }
+        }
+    }
+    echo json_encode(array('list' => $list));
 }
 
 function GetFriendList()
@@ -86,8 +113,23 @@ function GetFriendList()
     $player = GetPlayer();
     $playerId = $player->id;
     $list = array();
-    // TODO: Implement this
-    echo json_encode(array('list' => CursorsToArray($list)));
+    // TODO: Improve this
+    $db = \Base::instance()->get('DB');
+    $prefix = \Base::instance()->get('db_prefix');
+    $rows = $db->exec('SELECT ' . $prefix . 'player.id AS id, ' .
+        $prefix . 'player_friend.targetPlayerId AS targetPlayerId FROM ' .
+        $prefix . 'player RIGHT JOIN ' . $prefix . 'player_friend ON ' .
+        $prefix . 'player.id = ' . $prefix . 'player_friend.playerId WHERE '.
+        $prefix . 'player.id = :playerId',
+        array(':playerId' => $playerId));
+    // Add list
+    foreach ($rows as $row) {
+        $socialPlayer = GetSocialPlayer($playerId, $row['targetPlayerId']);
+        if ($socialPlayer) {
+            $list[] = $socialPlayer;
+        }
+    }
+    echo json_encode(array('list' => $list));
 }
 
 function GetFriendRequestList()
@@ -95,17 +137,46 @@ function GetFriendRequestList()
     $player = GetPlayer();
     $playerId = $player->id;
     $list = array();
-    // TODO: Implement this
-    echo json_encode(array('list' => CursorsToArray($list)));
+    // TODO: Improve this
+    $db = \Base::instance()->get('DB');
+    $prefix = \Base::instance()->get('db_prefix');
+    $rows = $db->exec('SELECT ' . $prefix . 'player.id AS id, ' .
+        $prefix . 'player_friend_request.targetPlayerId AS targetPlayerId FROM ' .
+        $prefix . 'player RIGHT JOIN ' . $prefix . 'player_friend_request ON ' .
+        $prefix . 'player.id = ' . $prefix . 'player_friend_request.playerId WHERE '.
+        $prefix . 'player_friend_request.playerId = :playerId',
+        array(':playerId' => $playerId));
+    // Add list
+    foreach ($rows as $row) {
+        $socialPlayer = GetSocialPlayer($playerId, $row['targetPlayerId']);
+        if ($socialPlayer) {
+            $list[] = $socialPlayer;
+        }
+    }
+    echo json_encode(array('list' => $list));
 }
 
 function GetOpponentList()
 {
     $player = GetPlayer();
     $playerId = $player->id;
+    $arenaScore = $player->arenaScore;
     $list = array();
-    // TODO: Implement this
-    echo json_encode(array('list' => CursorsToArray($list)));
+    // TODO: Improve this
+    $arenaScoreCap = $arenaScore + 100;
+    $limit = 25;
+    $rows = $db->exec('SELECT id FROM ' . $prefix . 'player WHERE ' . 
+        'profileName != "" AND ' . 
+        'arenaScore < ' . $arenaScoreCap . ' ' . 
+        'ORDER BY arenaScore DESC LIMIT ' . $limit);
+    foreach ($rows as $row) {
+        $socialPlayer = GetSocialPlayer($playerId, $row['id']);
+        if ($socialPlayer) {
+            $list[] = $socialPlayer;
+        }
+    }
+    // TODO: If opponents not enough, fill more (May be fake players)
+    echo json_encode(array('list' => $list));
 }
 
 function GetServiceTime()
