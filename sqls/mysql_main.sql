f1c06bb13899dfd05e9601faf49166052896a9a4
Insthync <ittipon.bay@gmail.com>
2021-05-06 15:41:25 +0700

    Add raid event ranking

--- sqls/mysql_main.sql
+++ sqls/mysql_main.sql
@@ -303,6 +303,22 @@ CREATE TABLE `<<__prefix__>>raid_event_creation` (
   `events` text NOT NULL
 ) ENGINE=InnoDB;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `<<__prefix__>>raid_event_ranking`
+--
+
+CREATE TABLE `<<__prefix__>>raid_event_ranking` (
+  `id` BIGINT NOT NULL AUTO_INCREMENT,
+  `playerId` BIGINT NOT NULL DEFAULT '0',
+  `eventId` BIGINT NOT NULL DEFAULT '0',
+  `damage` INT NOT NULL DEFAULT '0',
+  `createdAt` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updatedAt` DATETIME on update CURRENT_TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`), INDEX (`playerId`), INDEX (`eventId`)
+) ENGINE = InnoDB;
+
 --
 -- Indexes for dumped tables
 --
---
+++ sqls/update-1.17.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `<<__prefix__>>raid_event_ranking` (
+  `id` BIGINT NOT NULL AUTO_INCREMENT,
+  `playerId` BIGINT NOT NULL DEFAULT '0',
+  `eventId` BIGINT NOT NULL DEFAULT '0',
+  `damage` INT NOT NULL DEFAULT '0',
+  `createdAt` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updatedAt` DATETIME on update CURRENT_TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`), INDEX (`playerId`), INDEX (`eventId`)
+) ENGINE = InnoDB;
